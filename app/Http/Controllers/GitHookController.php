1008e3b570e3c66888868d4e6d29923456bebffa
hooli4 <hooli9un@yandex.ru>
2025-06-05 19:37:58 +0500

    correct commands

--- app/Http/Controllers/GitHookController.php
+++ app/Http/Controllers/GitHookController.php
@@ -27,35 +27,22 @@ class GitHookController extends Controller
         ]);
 
         try {
-            // Установим флаг выполнения обновления
             self::$isUpdating = true;
 
-            // Выполнение команд Git
             $this->executeGitCommands();
 
-            // Завершение работы
             return response()->json(['message' => 'Project updated successfully.']);
         } catch (\Exception $e) {
             Log::error('Error during Git update', ['error' => $e->getMessage()]);
             return response()->json(['message' => 'An error occurred during the update.'], 500);
         } finally {
-            // Сброс флага обновления
             self::$isUpdating = false;
         }
     }
 
     private function executeGitCommands() {
-        // Переключение на главную ветку (master/main)
         $output = [];
-        exec('git checkout main 2>&1', $output);
-        Log::info('Switched to main branch', ['output' => implode("\n", $output)]);
-
-        // Отмена всех изменений
-        exec('git reset --hard 2>&1', $output);
-        Log::info('All changes reverted', ['output' => implode("\n", $output)]);
-
-        // Обновление проекта
-        exec('git pull origin main 2>&1', $output);
-        Log::info('Project updated', ['output' => implode("\n", $output)]);
+        exec('cd ../../main && git reset --hard && git pull origin main', $output);
+        Log::info('Switched to main branch', ['output' => $output]);        
     }
 }
